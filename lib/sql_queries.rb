8090ee1755fa873178d24313ca8cb0c078c806be
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-14 19:19:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, SUM(pledges.amount) as total_raised 
   FROM projects 
-  JOIN pledeges 
+  JOIN pledges 
   ON project.id = pledges_project_id
   GROUP BY projects.id
   HAVING SUM(pledegs.amount) >= projects.funding_goal;"
