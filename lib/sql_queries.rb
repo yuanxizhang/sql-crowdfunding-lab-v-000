cc6a28b464b5881362d8e52f1a9fe6ba7f912169
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-14 18:18:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,11 +23,12 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, pledges.amount 
+  "SELECT projects.title, SUM(pledges.amount) as total_raised 
   FROM projects 
   JOIN pledeges 
-  ON project.id = pledges_project_id 
-  WHERE pledegs.amount >= projects.funding_goal;"
+  ON project.id = pledges_project_id
+  GROUP BY projects.id
+  HAVING SUM(pledegs.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
